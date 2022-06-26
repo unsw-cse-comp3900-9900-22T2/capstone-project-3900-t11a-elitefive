acef98b7da30d22daca68c55f974b40157d92df7
Yirong Shen <yirongshen@Yirongs-MacBook-Pro.local>
2022-06-26 18:51:42 +0700

    Updated Frontend Welcome Pages

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import Board from '../components/Board';
+import { Typography } from '@mui/material';
+import StyledInput from '../components/StyledInput';
+import {StyledButton} from '../components/ReusableButton-styled';
+import Button, {Button2, LargeButton} from '../components/ReusableButton';
+import {Routes, Route, useNavigate} from 'react-router-dom';
+import YavalathButton from '../components/YavalathButton';
 
 type Props = {}
 
+const Container  = styled.div`
+  min-height: 100vh;
+
+  display: flex;
+  flex-direction: Column;
+
+  justify-content: center;
+  align-items: center;
+  grid-gap: 30px;
+`
+
+const Container1  = styled.div`
+  min-height: 100vh;
+
+  display: flex;
+  flex-direction: row;
+
+  justify-content: center;
+  align-items: center;
+  grid-gap: 30px;
+`
+
 export default function Dashboard({}: Props) {
   return (
-    <div>Dashboard</div>
+    <Container>
+      <Container1>
+        <LargeButton>
+          <Typography variant="h3">{"Play AI"}</Typography>
+        </LargeButton>
+        <LargeButton>
+          <Typography variant="h3">{"Leaderboard"}</Typography>
+        </LargeButton>
+        <LargeButton>
+          <Typography variant="h3">{"Replays"}</Typography>
+        </LargeButton>
+      </Container1>
+    </Container>
   )
 }
--- frontend/src/pages/Gamepage.tsx
+++ frontend/src/pages/Gamepage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
+import {Routes, Route, useNavigate} from 'react-router-dom';
 
 // local
 import Board from '../components/Board';
@@ -12,6 +13,12 @@ const Container = styled.div`
 `;
 
 export default function Gamepage({}: Props) {
+  const navigate = useNavigate();
+  const navigateToGame = () => {
+    // 👇️ navigate to /contacts
+    navigate('/game');
+  };
+  
   return (
     <Container>
       <YavalathButton/>
