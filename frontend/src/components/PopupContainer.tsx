6cb520f204f38cbe93c8a99f5bf1db0e82492e52
Jacky <gokurohono@gmail.com>
2022-06-27 14:27:18 +1000

    initial popup

---
+++ frontend/src/components/PopupContainer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components';
+
+type Props = {}
+
+const Container = styled.div`
+  
+`;
+
+export default function PopupContainer({}: Props) {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
