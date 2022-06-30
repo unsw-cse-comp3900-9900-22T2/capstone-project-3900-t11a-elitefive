3ba0acf4466ca5815ed24fb6cf770cf981ff076a
Imanexx <davconnick@gmail.com>
2022-06-30 20:33:44 +1000

    Further refactoring and tab consistency

--- backend/src/game/basegame.cpp
+++ backend/src/game/basegame.cpp
@@ -7,24 +7,54 @@ BaseGame::BaseGame(int players)
 {}
 
 auto BaseGame::whose_turn() -> int {
-    return player_turn_;
+	return player_turn_;
 }
 auto BaseGame::num_moves() -> int {
-    return nmoves_;
+	return nmoves_;
 }
 
 auto BaseGame::board() -> Board & {
-    return board_;
+	return board_;
 }
 
 auto BaseGame::board() const -> Board const& {
-    return board_;
+	return board_;
 }
 
 auto BaseGame::increase_move() -> void {
-    ++nmoves_;
+	++nmoves_;
 }
 
 auto BaseGame::pass_turn() -> void {
-    player_turn_ = (player_turn_ + 1) % board().num_players();
+	player_turn_ = (player_turn_ + 1) % board().num_players();
+}
+
+
+auto BaseGame::indexToCoord(int index) -> std::string {
+	int q = calculate_q(index);
+	char letter = 'a' + q + 4;
+	int column = index - floor_index(q) + 1;
+	return letter + std::to_string(column);
+}
+
+auto BaseGame::coordToIndex(std::string coord) -> int {
+	int letter = coord[0];
+	if (letter >= 'a' && letter <= 'i') {
+		return floor_index(letter - 'a' - 4) + stoi(coord.substr(1)) - 1;
+	}
+	return -1;
+}
+
+// Private static functions
+auto BaseGame::floor_index(int q) -> int {
+	return ((q + 8) * (q + 9) / 2) - 10 - ((q > 1) ? q * (q - 1) : 0);
+}
+
+int BaseGame::calculate_q(int index) {
+	for (int q = -4; q < 5; ++q) {
+		if (index < floor_index(q + 1)) {
+			return q;
+		}
+	}
+	return -1;
 }
--- backend/src/game/include/basegame.hpp
+++ backend/src/game/include/basegame.hpp
@@ -7,63 +7,43 @@
 #include "board.hpp"
 
 class BaseGame {
-    private:
-        Board board_;
+	private:
+		Board board_;
 		int player_turn_;
 		int nmoves_;
-    public:
-        BaseGame(int players);
-        virtual ~BaseGame() = default;
+	public:
+		BaseGame(int players);
+		virtual ~BaseGame() = default;
 
-        auto virtual play(std::string move) -> bool = 0;
+		auto virtual play(std::string move) -> bool = 0;
 		auto virtual play(int index) -> bool = 0;
 		
 		auto whose_turn() -> int;
 		auto num_moves() -> int;
-        auto board() -> Board &;             // Normally you can just change the board
-        auto board() const -> Board const&;  // Needed for the '<<' operator
+		auto board() -> Board &;             // Normally you can just change the board
+		auto board() const -> Board const&;  // Needed for the '<<' operator
 
 
-        // auto status() -> Game::state;
-        auto increase_move() -> void;
-        auto pass_turn() -> void;
+		// auto status() -> Game::state;
+		auto increase_move() -> void;
+		auto pass_turn() -> void;
 
 		auto check() -> bool; // Valid move?
 		auto over() -> bool; // Has the game ended?
 
-        friend auto operator<<(std::ostream& os, BaseGame const& basegame) -> std::ostream& {
-            os << basegame.board();
-            return os;
-        }
-
-    private:
-		auto static floor_index(int q) -> int {
-			return ((q + 8) * (q + 9) / 2) - 10 - ((q > 1) ? q * (q - 1) : 0);
+		friend auto operator<<(std::ostream& os, BaseGame const& basegame) -> std::ostream& {
+			os << basegame.board();
+			return os;
 		}
 
-		int static calculate_q(int index) {
-			for (int q = -4; q < 5; ++q) {
-				if (index < floor_index(q + 1)) {
-					return q;
-				}
-			}
-			return -1;
-		}
-    public:
-        auto static indexToCoord(int index) -> std::string {
-			int q = calculate_q(index);
-			char letter = 'a' + q + 4;
-			int column = index - floor_index(q) + 1;
-			return letter + std::to_string(column);
-		}
+	private:
+		// Helper static functions that facilate the public static functions
+		auto static floor_index(int q) -> int;
+		auto static calculate_q(int index) -> int;
 
-		auto static coordToIndex(std::string coord) -> int {
-            int letter = coord[0];
-            if (letter >= 'a' && letter <= 'i') {
-                return floor_index(letter - 'a' - 4) + stoi(coord.substr(1)) - 1;
-            }
-            return -1;
-        }
+	public:
+		auto static indexToCoord(int index) -> std::string;
+		auto static coordToIndex(std::string coord) -> int;
 };
 
 #endif
