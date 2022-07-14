d62f5c694fc2d1b1f62c0f1ee49f27b3abc528b7
Jacky <gokurohono@gmail.com>
2022-07-14 22:33:51 +1000

    snapshot works visually

--- frontend/src/components/SnapshotPopup.tsx
+++ frontend/src/components/SnapshotPopup.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components';
 import { Modal } from '@mui/material';
+import SnapshotBoard from './SnapshotBoard';
+import Button from './ReusableButton';
 
 
 type Props = {
@@ -18,13 +20,36 @@ const Container = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+
+  display:flex;
 `;
+
+const Left = styled.div`
+  width: 80%;
+`;
+
+const Right = styled.div`
+  background: var(--accent-darker);
+  display:flex;
+  flex-direction:column;
+  padding: 2%;
+  padding-top: 20%;
+  
+`
+
 
 export default function SnapshotPopup({open, handleClose}: Props) {
   return (
     <Modal open={open} onClose={handleClose}>
       <Container>
-        hello
+        <Left>
+          <SnapshotBoard/>
+        </Left>
+        <Right>
+          <Button>
+            Confirm
+          </Button>
+        </Right>
       </Container>
     </Modal>
   )
