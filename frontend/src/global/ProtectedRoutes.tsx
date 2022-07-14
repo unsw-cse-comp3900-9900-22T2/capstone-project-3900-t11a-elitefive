adae80209d84e3391bd72097d674090790ee6ce3
Jacky <gokurohono@gmail.com>
2022-07-15 00:54:11 +1000

    works

--- frontend/src/global/ProtectedRoutes.tsx
+++ frontend/src/global/ProtectedRoutes.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import {
   Navigate,
   Outlet,
-  useNavigate
 } from 'react-router-dom';
 
 import { useAuth } from './GlobalAuth';
@@ -13,10 +12,10 @@ type Props = {
 
 export default function ProtectedRoutes({}: Props) {
   const { isLogged } = useAuth();
-  const navigate = useNavigate();
 
   if(!isLogged()) {
-    navigate('/');
+    console.log(isLogged());
+    return <Navigate to="/" replace/>
   }
   return <Outlet/>
 }
