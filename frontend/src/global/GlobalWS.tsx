f3e85afb5bd9c85d1621806216c3cefeda8125f2
Jacky <gokurohono@gmail.com>
2022-07-18 23:52:18 +1000

    name hack

--- frontend/src/global/GlobalWS.tsx
+++ frontend/src/global/GlobalWS.tsx
@@ -1,4 +1,6 @@
+import { getuid } from 'process';
 import React, { useContext, useEffect, useState } from 'react';
+import { useAuth } from './GlobalAuth';
 import { useGameState, PlayerType } from './GlobalGameState';
 
 interface payload {
@@ -30,14 +32,15 @@ export const WSProvider = ({ children, gameId }: Props) => {
   const [socket, setSocket] = useState<WebSocket| null>(null);
   const [socketConnected, setSocketConnected] = useState<boolean>(false);
   
-  const { playMove, setHexTileState, playerJoin, getPlayerInfo, setWinner } = useGameState();
-  
+  const { playMove, setHexTileState, playerJoin, getPlayerInfo, setWinner, setPlayerName } = useGameState();
+  const { getUID } = useAuth();
   
   useEffect(() => {
     const WS = new WebSocket(`ws://localhost:8080/ws/game/${gameId}`);
     WS.onopen = () => {
       setSocketConnected(true);
       playerJoin({uid: "abc", color: "red"});
+      playerJoin({uid: "BOT", color: "blue"});
     }
     WS.onmessage = (message) => {
       const payload:payload = JSON.parse(message.data) as payload
@@ -49,7 +52,7 @@ export const WSProvider = ({ children, gameId }: Props) => {
           if(move) {
             // this is hardcoded for SINGLE PLAYER will need
             // backend player join broadcast to correctly work
-            playerJoin({uid: "BOT", color: "blue"});
+            // playerJoin({uid: "BOT", color: "blue"});
             const playerColor = getPlayerInfo("BOT")?.color;
             if(!playerColor) break;
             setHexTileState(move, {
@@ -78,8 +81,9 @@ export const WSProvider = ({ children, gameId }: Props) => {
         }
         case "player_names": {
           const payload = JSON.parse(message.data);
-          const { player_names } = payload;
-          console.log(player_names);
+          const { player_name } = payload;
+          setPlayerName(0, player_name[0]);
+          setPlayerName(1, player_name[1]);
           break;
         }
       }
