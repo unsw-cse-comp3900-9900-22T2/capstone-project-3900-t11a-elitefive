a3d13490bc3c0fc7ba4eef07d2f606cdfb1cdd10
sagebee <sagebarreda@gmail.com>
2022-08-03 15:21:32 +1000

    hi

--- build/backend/src/server/server_util.cpp
+++ build/backend/src/server/server_util.cpp
@@ -64,8 +64,8 @@ auto send_email_welcome(std::string email, std::string username) -> void{
 	auto message = std::string("Hello ") + username + std::string("!! \\n")
 	+ std::string("Account creation successful.\\nWelcome to Yavalath!!");
 
-    auto wd = std::filesystem::current_path();
-	auto cmd = wd.c_str() + "/mail/send_email.sh " + email + " \"Welcome to Yavalath!\" \"" + message + " \"";
+    auto wd = std::filesystem::current_path().c_str;
+	auto cmd = wd + "/mail/send_email.sh " + email + " \"Welcome to Yavalath!\" \"" + message + " \"";
 		
 	std::system(cmd.c_str());
 }
