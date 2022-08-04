d1e26f858fa037eb1303740e958c529da2ef4a09
Imanexx <46551538+Imanexx@users.noreply.github.com>
2022-08-04 15:23:45 +1000

    Update install.sh

--- build/install.sh
+++ build/install.sh
@@ -1,23 +1,23 @@
-#!/bin/sh
+#!/usr/bin/sh
  
 wd=$(pwd)
 fedir="${wd}/frontend"
 bedir="${wd}/backend"
 dbdir="${wd}/database"
  
-apt-get update
+apt-get -y update
 
 apt install -y cmake
 apt install -y libpq-dev
 apt install -y libpqxx-dev
-apt install zlib1g
-apt install zlib1g-dev
- 
+apt install -y zlib1g
+apt install -y zlib1g-dev
+apt install -y libssl-dev
 apt install -y g++
  
 # Set up database
 cd "$dbdir"
-sh db_setup.sh
+sh pg_setup.sh
 
 # Set up backend
 cd "$bedir"
