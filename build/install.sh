d57d87220b5a783b76680ec3a7ccdd39efbd003f
Imanexx <46551538+Imanexx@users.noreply.github.com>
2022-08-04 16:30:41 +1000

    Update install.sh

--- build/install.sh
+++ build/install.sh
@@ -34,7 +34,9 @@ make
 cd "$fedir"
 apt install curl
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-source /home/lubuntu/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install node
 npm install
 sh qterminal -e npm start
